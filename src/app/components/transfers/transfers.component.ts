66edf0517ddebaef57bff3ed8eeb3cb6b3aeaf66
DESKTOP-8PDS7D5\user <DESKTOP-8PDS7D5\user@gmail.com>
2025-06-09 17:36:04 +0800

    1.頁首記帳滑鼠圖標 2.刪除頁面標題區域 3.記帳頁排版和美編 4.轉帳頁撰寫

--- src/app/components/transfers/transfers.component.ts
+++ src/app/components/transfers/transfers.component.ts
@@ -4,10 +4,11 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-transfers',
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatIconModule,MatSelectModule],
   providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './transfers.component.html',
